2a4258df0b1e3e60400c603e44e8a0f01a560bb5
Thiago Vinicius Soares <1055485@sga.pucminas.br>
2018-04-17 21:33:33 -0300

    Ajuste do mincemeat

--- exerc22.py
+++ exerc22.py
@@ -32,6 +32,6 @@ s.reducefn = reducefn
 
 results = s.run_server(password="p4ssw0rd")
 
-w = csv.writer(open("C:\\Temp\\Arq_Exerc\\result.csv", "w"))
+w = csv.writer(open("C:\\Temp\\Join\\result.csv", "w"))
 for k, v in results.items():
     w.writerow([k, v])
